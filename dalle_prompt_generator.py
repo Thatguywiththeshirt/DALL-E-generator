8de929e32c9a795f9779fb19c260912e9667957f
Thatguywiththeshirt <129647138+Thatguywiththeshirt@users.noreply.github.com>
2023-04-09 17:40:16 -0500

    Create dalle_prompt_generator.py

---
+++ dalle_prompt_generator.py
@@ -0,0 +1,64 @@
+import json
+
+import random
+
+def load_data(filename):
+
+    try:
+
+        with open(filename, 'r') as file:
+
+            data = json.load(file)
+
+        return data
+
+    except FileNotFoundError:
+
+        print(f"Error: {filename} not found.")
+
+        return None
+
+    except json.JSONDecodeError:
+
+        print(f"Error: Invalid JSON in {filename}.")
+
+        return None
+
+def generate_dalle_prompt(data):
+
+    orientation = random.choice(data['orientations'])
+
+    artist = random.choice(data['artists'])
+
+    obj = random.choice(data['objects'])
+
+    prompt = f'{orientation} in the style of {artist} featuring a {obj}'
+
+    return prompt
+
+def main(data, num_prompts):
+
+    for _ in range(num_prompts):
+
+        print(generate_dalle_prompt(data))
+
+if __name__ == '__main__':
+
+    data = load_data('dalle_prompt_data.json')
+
+    if data:
+
+        try:
+
+            num_prompts = int(input('Enter the number of prompts to generate: '))
+
+            if num_prompts < 1:
+
+                raise ValueError
+
+            main(data, num_prompts)
+
+        except ValueError:
+
+            print("Error: Please enter a positive integer.")
+
